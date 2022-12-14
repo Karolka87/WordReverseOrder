0f3cecdfcb2585a7ef50f5bb4d3c386061e4c9e6
Karolka87 <115976221+Karolka87@users.noreply.github.com>
2022-12-14 20:00:11 +0100

    jam ci to wygooglowala i wlasnorecznie przepisala, ale do obgadania, bo tego reva nie do konca rozumiem

--- src/WordReverseOrder.java
+++ src/WordReverseOrder.java
@@ -6,11 +6,11 @@ public class WordReverseOrder {
         Scanner scan = new Scanner(System.in);
         String sentence = scan.nextLine();
         String[] arrOfStr = sentence.split(" ");
+        String rev = "";
 
-        for(String a : arrOfStr) {
-            System.out.println(a);
-
+        for(int i = 0; i < arrOfStr.length; i++) {
+            rev = arrOfStr[i] + " " + rev;
         }
-
+        System.out.println("Reversed sentence: " + rev);
     }
 }
